a6e5bb6bf1d6f0f932b6027671da944e790252fd
Hands On <handson959@gmail.com>
2013-02-09 00:25:13 -0800

    Changed index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head></head>
+<body>
+<h1>
 <?php
 echo "Hi! This is a video of Hands On"
 ?>
+</h1>
+</body>
+</html>
