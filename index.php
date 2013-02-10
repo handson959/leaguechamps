4dfc39901b4215e72451d71a4c62a5b38692c2f0
Hands On <handson959@gmail.com>
2013-02-10 00:49:23 -0800

    added db connectivity

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php echo "Hi There! This is Hands On 959";
 
 $con = mysql_connect("localhost", "admin", "Youtube@libra17");
-mysql_select_db("handson959", $con);
+if (!$con) {
+		die('Could not connect: ' . mysql_error());
+	}
+$db_select = mysql_select_db("handson959", $con);
+if (!$db_select) {
+		die('Database selection failed: ' . mysql_error());
+	}
 
 $query= "SELECT NAME FROM USER WHERE USERD = 102";
 $getUser_query = mysql_query($query);
