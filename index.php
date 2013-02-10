e4c77675269429a3a1d4b6a5a7f980ed7dc5db1e
Hands On <handson959@gmail.com>
2013-02-10 00:42:51 -0800

    added db connectivity

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php echo "Hi There! This is Hands On 959"; ?>
+<?php echo "Hi There! This is Hands On 959";
+
+mysql_connect("localhost", "admin", "Youtube@libra17");
+mysql_select_db("handson959", $con);
+
+$query= "SELECT NAME FROM USER WHERE USERD = 102";
+$getUser_query = mysql_query($query);
+$user = mysql_fetch_array($getUser_query);
+
+echo $user['NAME'];	
+?>
+
