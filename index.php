6518838f635d01e57df7f4c7b59d26ab3a0b0923
Hands On <handson959@gmail.com>
2013-02-10 00:59:03 -0800

    adding db connectivity

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php echo "Hi There! This is Hands On 959";
 
-$con = mysql_connect("instance38538.db.xeround.com:6511", "admin", "Youtube@libra17");
-if (!$con) {
-		die('Could not connect: ' . mysql_error());
-	}
-$db_select = mysql_select_db("handson959", $con);
-if (!$db_select) {
-		die('Database selection failed: ' . mysql_error());
-	}
+$con = mysql_connect("instance38538.db.xeround.com:6511", "handson959", "Youtube@libra17");
+mysql_select_db("handson959", $con);
 
 $query= "SELECT NAME FROM USER WHERE USERD = 102";
 $getUser_query = mysql_query($query);
